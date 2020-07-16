19b20bd5be9b3687efb4665a593c8665c31684a5
Meeta 14 <meetamalviya14@gmail.com>
2020-07-17 01:14:13 +0530

    dest panel

--- visual/js/panel.js
+++ visual/js/panel.js
@@ -7,7 +7,7 @@ var Panel = {
 
         $('.panel').draggable();
         $('.accordion').accordion({
-            collapsible: false,
+            collapsible: true,
         });
         $('.option_label').click(function() {
             $(this).prev().click();
@@ -18,9 +18,9 @@ var Panel = {
         $('#play_panel').css({
             top: $algo.offset().top + $algo.outerHeight() + 20
         });
-        $('#NoOfDest').css({
-            top: $('#instructions_panel').offset().top + $('#instructions_panel').outerHeight() + 20
-        });
+        // $('#NoOfDest').css({
+        //     top: $('#instructions_panel').offset().top + $('#instructions_panel').outerHeight() + 20
+        // });
         $('#button2').attr('disabled', 'disabled');
         // $('#block_panel').css({
         //     top: $algo.offset().top + $algo.outerHeight() + 100
@@ -116,13 +116,12 @@ var Panel = {
         }
 
         return finder;
-    }
+    },
 
-    // getTerrain: function(){
-    //     selected_header = $(
-    //         '#play_panel '+
-    //         '.ui-accordion-header[aria-selected=true]'
-    //     ).attr('id');
-    //     return this.id
-    // }
+    getnumdest: fuction(){
+        selected_dest =typeof $('#NoOfDest' +
+                                 '.option_label').val() !== 'undefined';
+        console.log(selected_dest);
+        return selected_dest;
+    },
 };

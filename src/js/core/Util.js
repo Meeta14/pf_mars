d85ae6445ff6e6964d7167edc4e569267ecf957b
Meeta Malviya <46567151+Meeta14@users.noreply.github.com>
2020-07-24 20:14:18 +0530

    remove redundant functions

--- src/js/core/Util.js
+++ src/js/core/Util.js
@@ -1,36 +1,4 @@
-
-// function to return path from the end node
-function backtrace2(cellDetails,node){
-	var path=[[node.x,node.y]];
-	x=node.x;
-	y=node.y;
-	while(!( cellDetails[x][y].parent_i == x && cellDetails[x][y].parent_j==y )){
-		tempx=cellDetails[x][y].parent_i;
-		tempy=cellDetails[x][y].parent_j;
-		x=tempx
-		y=tempy
-		path.push([x,y])
-
-	}
-		return path.reverse();
-}
-
-exports.backtrace2=backtrace2;
-
-function pathLength(path) {
-// change // TODO: add terrain path
-var i, sum = 0, a, b, dx, dy;
-for (i = 1; i < path.length; ++i) {
-		a = path[i - 1];
-		b = path[i];
-		dx = a[0] - b[0];
-		dy = a[1] - b[1];
-		sum += Math.sqrt(dx * dx + dy * dy);
-}
-return sum;
-}
-exports.pathLength = pathLength;
-
+//backtrance func
 function backtrace(cellDetails,node){
     var path=[[node.x,node.y]];
     temp=Object.assign({},node);
